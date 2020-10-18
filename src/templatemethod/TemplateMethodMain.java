fee0354472398398b2dde658c682e29df4ea4841
adrianscorus14 <adrianscorus14@gmail.com>
2020-10-18 17:49:28 +0300

    Initial commit

---
+++ src/templatemethod/TemplateMethodMain.java
@@ -0,0 +1,29 @@
+package templatemethod;
+
+import templatemethod.example.Capriciosa;
+import templatemethod.example.Margharita;
+import templatemethod.example.Pizza;
+import templatemethod.exercise.BubbleSort;
+import templatemethod.exercise.BucketSort;
+import templatemethod.exercise.Sort;
+
+public class TemplateMethodMain {
+    public static void main(String[] args) {
+        Pizza margharita = new Margharita();
+        Pizza capriciosa = new Capriciosa();
+        margharita.bakingPizza();
+        System.out.println("-----------------");
+        capriciosa.bakingPizza();
+
+//        System.out.println("-----------------");
+//        Pizza fruttiDiMare = new FruttiDiMare();
+//        fruttiDiMare.bakingPizza();
+//
+        Sort bubbleSort = new BubbleSort();
+        Sort bucketSort = new BucketSort();
+
+        bubbleSort.sortArray(10);
+        System.out.println("-----------------");
+        bucketSort.sortArray(10);
+    }
+}
